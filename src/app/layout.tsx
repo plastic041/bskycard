f392914c56544c5cecf6243b41544b9d2ae4a895
plastic041@ <plastic041@gmail.com>
2024-11-24 00:14:50 +0900

    google site verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="5PXIoBe2lYSYORu-zYYU2u4qP7Gwl3LwgxWS3Gjjc3I"
+        />
+      </head>
       <body className="antialiased h-svh flex flex-col container mx-auto">
         {children}
 
